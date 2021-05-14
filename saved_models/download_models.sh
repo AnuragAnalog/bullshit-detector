378f4becd83d3bec4805414ad732d7d331e4338f
AnuragAnalog <anurag.peddi1998@gmail.com>
2021-05-14 14:22:18 +0530

    added saved model and download scripts

---
+++ saved_models/download_models.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -O "tfidf_ensemble.pkl" "https://archive.org/download/bull-shit-detector-tfidf-ensemble/tfidf_ensemble.pkl"
+wget -O "count_ensemble.pkl" "https://archive.org/download/bullshit-detector-count-ensemble/count_ensemble.pkl"
